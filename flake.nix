f59e18c329f832d2564747987d892aa1ca426669
Arlind Sulejmani <arlind@sulej.ch>
2025-08-29 15:14:10 +0200

    pin wsl version

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/release-25.05";
     nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
   };
 
@@ -33,9 +33,7 @@
         specialArgs = { inherit inputs; };
         modules = [
           nixos-wsl.nixosModules.default
-
           ./nixos/configuration.nix
-
           {
             system.stateVersion = "25.05";
             wsl.enable = true;
