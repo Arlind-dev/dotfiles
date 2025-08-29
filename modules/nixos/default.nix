b8497ffb88687278908a150bc65bc006764f88c5
Arlind Sulejmani <arlind@sulej.ch>
2025-08-29 15:21:50 +0200

    add system packages

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,2 +1,17 @@
+{ config, pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    tree
+    unzip
+    ripgrep
+    htop
+    fastfetch
+    wget
+    tcpdump
+    bat
+    jq
+    zip
+    gh
+    dig
+  ];
 }
